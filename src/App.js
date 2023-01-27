e0d7fa6beb700655c4dd3fef62bec59ecc255edf
Bibek Chalise <bibekchalise@Bibeks-MacBook-Air.local>
2023-01-27 16:47:45 +0545

    doctor wala kam

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Intro from './components/intro/intro';
 import Landing from './components/landing/landing';
+import ReportDoc from './components/ReportDoctor/reportdoc';
 
 function App() {
   return (
     <div className="App">
       <Intro/>
       <Landing/>
+      <ReportDoc />
     </div>
   );
 }
